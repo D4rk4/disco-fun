607d4657f640daf2a069a9cbb3f680847d402577
Dmitry <h31337@gmail.com>
2018-04-02 07:35:33 +0200

    If file missing, try another
    Dirty hack, need cycle control!

--- disconnect.php
+++ disconnect.php
@@ -1,14 +1,18 @@
 <?php
-$dir = "/var/www/html/disconnect";
-$filename = rand(1, 7999);
-$file = $dir."/".$filename.".au";
 
-
-if(file_exists($file)){
-    header('Content-type: audio/basic');
-    header('Content-length: ' . filesize($file));
-    header('Cache-Control: no-cache');
-    readfile($file);
-}else{
-    header("HTTP/1.0 404 Not Found");
+function stream () {
+        $dir = "/var/www/html/disconnect";
+        $filename = rand(1, 7999);
+        $file = $dir."/".$filename.".au";
+        if(file_exists($file)){
+                header('Content-type: audio/basic');
+                header('Content-length: ' . filesize($file));
+                header('Cache-Control: no-cache');
+                readfile($file);
+        }else{
+                stream();
+                //header("HTTP/1.0 404 Not Found");
+        }
 }
+
+stream();
